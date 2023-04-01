6d4922fd33d812189967cfdacdedb89c0916c5c8
Mauricio Reyes Campos <mreyesc@gmail.com>
2023-04-01 18:34:07 -0500

    Login pages final version

---
+++ src/login.js
@@ -0,0 +1,8 @@
+localStorage.setItem("userStorage", "mreyes");
+localStorage.setItem("passStorage", "1234");
+
+function loginValidation() {
+    var userSave
+    const user = document.getElementById("validationUserName");
+    const pass = document.getElementById("validationPassword");
+}
