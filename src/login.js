272a56abfd0f63cd9990bcb6f12351fa8a4fd494
Mauricio Reyes Campos <mreyesc@gmail.com>
2023-04-03 08:17:40 -0500

    Login validation updated

--- src/login.js
+++ src/login.js
@@ -1,8 +1,37 @@
-localStorage.setItem("userStorage", "mreyes");
-localStorage.setItem("passStorage", "1234");
+sessionStorage.setItem("userSave", "user");
+sessionStorage.setItem("passSave", "pass");
 
-function loginValidation() {
-    var userSave
-    const user = document.getElementById("validationUserName");
-    const pass = document.getElementById("validationPassword");
+function checkLogin(event) {
+  event.preventDefault();
+  var userStorage = sessionStorage.getItem("userSave");
+  console.log(userStorage);
+  var passStorage = sessionStorage.getItem("passSave");
+  console.log(passStorage)
+  var loginName = document.getElementById("userName");
+  var loginpass = document.getElementById("userPassword");
+  if (loginName.value === userStorage && loginpass.value === passStorage) {
+    alert("Correcto");
+  } else {
+    alert("Incorrecto");
+  }
 }
+
+
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
